d00123576b8490268b89801ee438ee2fba8a89c9
JenilJaiswal <jeniljaiswal14@gmail.com>
2025-06-23 01:29:38 +0530

    Add the Siri Wave and Voice Assistant for the initializing

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 import eel
+from backend.feature import *
 
 eel.init('frontend')
+
+playAssistantSound()
+
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
 eel.start('index.html',mode=None,host='localhost', block=True)
