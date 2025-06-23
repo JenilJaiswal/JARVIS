0666d417c8635eef46e47c38c3446f3a4713770d
JenilJaiswal <jeniljaiswal14@gmail.com>
2025-06-23 22:58:01 +0530

    Speech recognition and and audio to text

--- backend/command.py
+++ backend/command.py
@@ -1,4 +1,6 @@
 import pyttsx3
+import speech_recognition as sr
+
 
 def speak(text):
     engine = pyttsx3.init()
@@ -9,4 +11,28 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
     
-speak("Hello, I am JARVIS How can I assist you today?")
+def takecommand():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('listening....')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print('recognizing')
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+
+text = takecommand()
+
+speak(text)
+# speak("Hello, I am JARVIS How can I assist you today?")
